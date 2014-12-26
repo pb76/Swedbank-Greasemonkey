7073a68cd3b38fefae4295135be6543cde58e31c
Patrik Bernhardsson <pb76@users.noreply.github.com>
2014-12-26 16:03:50 +0100

    Update swedbank.js
    Added which options is available.

--- swedbank.js
+++ swedbank.js
@@ -3,7 +3,14 @@
 // @namespace  https://internetbank.swedbank.se/      
 // @include    https://internetbank.swedbank.se/* 
 // ==/UserScript==
+//Enter your social security number (personnummer) instead of 12345-1234
 document.getElementById("auth:kundnummer").value = "123456-1234";
+//Changes drop down select to "Mobilt BankID".
+//ACTCARD = Säkerhetsdosa
+//MOBILBID = Mobilt BankID
+//BIDKORT = BankID på kort
+//PIN6 = Personlig kod
+//FTGKOPPL = Företagskopplad dosa
 var val = "MOBILBID";
 var sel = document.getElementById('auth:metod_2');
 var opts = sel.options;
